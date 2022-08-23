d9b56483bbaf327b3c4c28c006460f1a56ef7666
Will Foster <willf@remitly.com>
2022-08-22 22:12:48 -0600

    fix(home): re-enabled the static home page on the lambda site

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
+import serverlessExpress from "@vendia/serverless-express";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import { ApolloServer } from "apollo-server-lambda";
 import { resolvers } from "./resolvers";
 import { typeDefs } from "./schema";
 
+const path = require("path");
+const express = require("express");
+
+// to serve /graphql
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -10,5 +15,11 @@ const server = new ApolloServer({
   cache: "bounded",
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
+export const graphql = server.createHandler();
 
-export const graphqlHandler = server.createHandler();
+// to serve /
+const app = express();
+app.use(express.static(path.join(__dirname, "static")));
+export const home = serverlessExpress({
+  app,
+});
